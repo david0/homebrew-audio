cded0381cf971d2305c9035ef2d71cd999aa45cb
David <ottodavid@gmx.net>
2016-01-04 19:11:00 +0100

    swh-lv2: add version 1.0.16

--- Formula/swh-lv2.rb
+++ Formula/swh-lv2.rb
@@ -1,6 +1,8 @@
 class SwhLv2 < Formula
   desc "SWH Plugins in LV2 format"
   homepage "http://plugin.org.uk/"
+  url "https://github.com/swh/lv2/archive/v1.0.16.tar.gz"
+  sha256 "bc24512de6e2fb7a493226e2e01a80ba8462a318b15c3b0fd0cd914b018c3548"
   head "https://github.com/swh/lv2.git"
 
   depends_on "pkg-config" => :build
