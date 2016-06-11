5118259ba4eba3437a998bc0a613016b592813ed
David <ottodavid@gmx.net>
2016-06-11 14:26:43 +0200

    ardour4: Fix compiling on El Capitan

--- Formula/ardour4.rb
+++ Formula/ardour4.rb
@@ -26,7 +26,7 @@ class Ardour4 < Formula
   depends_on "suil" => :recommended
   depends_on "fftw"
   depends_on "lilv"
-  depends_on "libsigc++" # can be removed when libsigc++ > 2.6.2
+  depends_on "libsigc++"
   depends_on :x11 => :optional
 
   depends_on "python3" => :build # for fix-installnames-magic
@@ -40,29 +40,36 @@ class Ardour4 < Formula
     sha256 "2d9c9220a589709387f123107e2ecc555d51ed35f1da64194cb8a95e0d902069"
   end
 
+  # Otherwise fails with /System/Library/Frameworks/Security.framework/Headers/CSCommon.h:200:32: error: enumerator value evaluates to -2147483648, which cannot be narrowed to type 'uint32_t'
+  #    (aka 'unsigned int') [-Wc++11-narrowing]
+  patch :DATA
+
   needs :cxx11
   def install
     ENV.cxx11
 
     if build.head?
-      system "git", "tag", "-a", "-m", "head tag", "4.4"
+      system "git", "tag", "-a", "-m", "head tag", "4.7"
     end
 
     (buildpath/"libs/ardour/revision.cc").write <<-EOS.undent
       #include "ardour/revision.h"
-      namespace ARDOUR { const char* revision = "4.4-999"; }
+      namespace ARDOUR { const char* revision = "4.7-999"; }
     EOS
+
+    #inreplace "wscript", "--stdlib=libc++", "--stdlib=libc++ --Wno-c++11-narrowing" 
 
     system "./waf", "configure", "--prefix=#{prefix}", "--with-backends=coreaudio,jack", "--use-libc++"
     system "./waf", "install"
+  
 
     # hack: fix library names by looking for libraries that do not exist after removing them from sources
     system "rm", "-rf", "build"
     resource("fix-installnames-magic").stage do
       chmod "+x", "fix-installnames-magic.py"
 
-      system "find " + prefix + ' \( -name ardour-4.4\* -or -name \*.dylib \) -exec chmod +w {} \;'
-      system "find " + prefix + ' \( -name ardour-4.4\* -or -name \*.dylib \) -exec ./fix-installnames-magic.py {} ' + prefix + ' \;'
+      system "find " + prefix + ' \( -name ardour-4.\* -or -name \*.dylib \) -exec chmod +w {} \;'
+      system "find " + prefix + ' \( -name ardour-4.\* -or -name \*.dylib \) -exec ./fix-installnames-magic.py {} ' + prefix + ' \;'
     end
 
     cd "tools/osx_packaging" do
@@ -76,3 +83,18 @@ class Ardour4 < Formula
     system "ardour4", "--help"
   end
 end
+
+
+__END__
+diff --git a/wscript b/wscript
+index 965e546..6bc0351 100644
+--- a/wscript
++++ b/wscript
+@@ -396,6 +396,7 @@ int main() { return 0; }''',
+ 
+     if opt.use_libcpp or conf.env['build_host'] in [ 'el_capitan' ]:
+        cxx_flags.append('--stdlib=libc++')
++       cxx_flags.append('-Wno-c++11-narrowing')
+        linker_flags.append('--stdlib=libc++')
+ 
+     if conf.options.cxx11 or conf.env['build_host'] in [ 'mavericks', 'yosemite', 'el_capitan' ]:
