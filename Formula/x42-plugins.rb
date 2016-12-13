a453b6f0aedf6ad7c7ec283a961c53396a5ae032
David <ottodavid@gmx.net>
2016-12-13 19:36:00 +0100

    x42-plugins: fix compile and add new plugins

--- Formula/x42-plugins.rb
+++ Formula/x42-plugins.rb
@@ -17,17 +17,25 @@ class X42Plugins < Formula
   depends_on "lv2"
   depends_on "ftgl" => :optional
   depends_on "gtk+" => :optional
+  depends_on "fftw"
 
   def install
     # https://github.com/x42/tuna.lv2/issues/3
     ENV.prepend "PATH", Formula["coreutils"].libexec/"gnubin" + ":"
+    ENV.append_to_cflags "-I" + Formula["zita-convolver"].include
+    ENV.append_to_cflags "-L" + Formula["zita-convolver"].lib
+    ENV.append_to_cflags "-I" + Formula["fftw"].include
+    ENV.append_to_cflags "-L" + Formula["fftw"].lib
+    ENV.append_to_cflags "-I" + Formula["cairo"].include
+    ENV.append_to_cflags "-L" + Formula["cairo"].lib
+
 
     if build.head?
       system "git", "submodule", "foreach", "git", "pull", "origin", "master"
     end
 
     # TODO: remove SUBDIRS but meters.lv2 is not yet building
-    system "make", "install", "PREFIX=#{prefix}", "SUBDIRS=balance.lv2 convoLV2 nodelay.lv2 xfade.lv2 midifilter.lv2  sisco.lv2 tuna.lv2 onsettrigger.lv2 mixtri.lv2 fil4.lv2"
+    system "make", "install", "PREFIX=#{prefix}", "SUBDIRS=balance.lv2 convoLV2 nodelay.lv2 xfade.lv2 midifilter.lv2  sisco.lv2 tuna.lv2 onsettrigger.lv2 mixtri.lv2 fil4.lv2 fat1.lv2 stepseq.lv2 midigen.lv2 testsignal.lv2 midimap.lv2 controlfilter.lv2"
   end
 
   test do
