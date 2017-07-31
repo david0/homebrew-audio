b265a7994fc54e6d5ba4d21b4f780b71cfa71823
Thomas Karmann <thomas@krmnn.de>
2017-07-31 14:22:05 +0200

    ardour5: Version bump to 5.10 * update ardour version to 5.10 * add missing dependencies boost & libarchive * add sha256sums

--- Formula/ardour5.rb
+++ Formula/ardour5.rb
@@ -1,10 +1,10 @@
 class Ardour5 < Formula
-  desc "hard disk recorder and digital audio workstation application"
-  homepage "http://ardour.org"
-  url "git://git.ardour.org/ardour/ardour.git", :tag=>'5.0'
-  #sha256 "598fb7fbe288caad6d4472f6b2057e15d9e6a66770a5e64c66af01b8cd7d14f3"
+  desc "Hard disk recorder and digital audio workstation application"
+  homepage "https://ardour.org"
+  url "git://git.ardour.org/ardour/ardour.git", :tag=>"5.10", :revision => "9c629c0c76808cc3e8f05e43bc760f849566dce6"
   head "git://git.ardour.org/ardour/ardour.git"
 
+  depends_on "boost"
   depends_on "cairo"
   depends_on "gtk+" #=> ["with-quartz-relocation"] # will need the ardour patch also most likely
   depends_on "gdk-pixbuf" #=> ["with-relocations"]
@@ -12,6 +12,7 @@ class Ardour5 < Formula
 
   depends_on "glibmm"
   depends_on "gtkmm"
+  depends_on "libarchive"
   depends_on "libsndfile"
   depends_on "liblo"
   depends_on "taglib"
@@ -33,10 +34,12 @@ class Ardour5 < Formula
 
   resource "fix-installnames-magic" do
     url "https://gist.githubusercontent.com/david0/34d1bbd280610ee48255/raw/234fec0d34a9b929f782c991f2c3804b85db6f9e/fix-installnames-magic.py"
+    sha256 "2fb1b4a3e8c30e397c05480bad770101e10db04713f5c30dc951a52f3a465e1f"
   end
 
   resource "mkappbundle" do
     url "https://gist.githubusercontent.com/david0/56ee00434e4693852c24/raw/ae47c87874c398378402e83688bc6f47fe86e83c/mkappbundle"
+    sha256 "ef5d349e9281605bc115217b146c81e6e38aefb39b014139e859ba031460b838"
   end
 
   # Otherwise fails with /System/Library/Frameworks/Security.framework/Headers/CSCommon.h:200:32: error: enumerator value evaluates to -2147483648, which cannot be narrowed to type 'uint32_t'
@@ -48,21 +51,20 @@ class Ardour5 < Formula
     ENV.cxx11
 
     if build.head?
-      system "git", "tag", "-a", "-m", "head tag", "5.0"
+      system "git", "tag", "-a", "-m", "head tag", "5.10"
     end
 
     (buildpath/"libs/ardour/revision.cc").write <<-EOS.undent
       #include "ardour/revision.h"
-      namespace ARDOUR { const char* revision = "5.0-999"; }
+      namespace ARDOUR { const char* revision = "5.10-999"; }
     EOS
 
-    #inreplace "wscript", "--stdlib=libc++", "--stdlib=libc++ --Wno-c++11-narrowing" 
+    # inreplace "wscript", "--stdlib=libc++", "--stdlib=libc++ --Wno-c++11-narrowing"
 
     system "./waf", "configure", "--prefix=#{prefix}", "--with-backends=coreaudio,jack", "--use-libc++"
     system "./waf", "install"
-  
 
-    # hack: fix library names by looking for libraries that do not exist after removing them from sources
+    # HACK: fix library names by looking for libraries that do not exist after removing them from sources
     system "rm", "-rf", "build"
     resource("fix-installnames-magic").stage do
       chmod "+x", "fix-installnames-magic.py"
@@ -79,7 +81,7 @@ class Ardour5 < Formula
   end
 
   test do
-    system "ardour5", "--help"
+    system "#{bin}/ardour5", "--help"
   end
 end
 
