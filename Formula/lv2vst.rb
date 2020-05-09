df0f5792fb4bc004f7fd71fb7af2582e22867232
David <ottodavid@gmx.net>
2020-05-09 07:29:16 +0200

    lv2vst: Fix build
    Fixes #21

--- Formula/lv2vst.rb
+++ Formula/lv2vst.rb
@@ -7,15 +7,16 @@ class Lv2vst < Formula
 
   def install
     ENV.deparallelize
-    ENV.permit_arch_flags
 
+    inreplace "Makefile", "$(VSTNAME).x86_64.dylib $(VSTNAME).i386.dylib", "$(VSTNAME).x86_64.dylib"
     inreplace "./include/lilv_config.h", 'LILV_DEFAULT_LV2_PATH "', 'LILV_DEFAULT_LV2_PATH "/usr/local/lib/lv2:'
-    system "make", "osxbundle"
-    mkdir_p "#{lib}/vst/lv2vst.vst"
-    lib.install "lv2.vst" => "vst/lv2.vst"
+    system "make", "STRIP=", "osxbundle"
+    mkdir_p "#{lib}/vst/"
+    lib.install "lv2.vst" => "#{lib}/vst/lv2.vst"
   end
 
   test do
     assert_match /_VSTPluginMain/, shell_output("nm  #{lib}/vst/lv2.vst/Contents/MacOS/lv2vst")
   end
 end
+
