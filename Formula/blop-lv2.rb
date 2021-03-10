f4f05b485d02fe407b271b63e6092e0d2a859ebe
David <ottodavid@gmx.net>
2021-03-10 21:31:47 +0100

    blob-lv2: version 1.0.2

--- Formula/blop-lv2.rb
+++ Formula/blop-lv2.rb
@@ -1,14 +1,14 @@
 class BlopLv2 < Formula
   desc "LV2 port of the BLOP LADSPA plugins by Mike Rawes"
   homepage "http://drobilla.net/software/blop-lv2/"
-  url "http://download.drobilla.net/blop-lv2-1.0.0.tar.bz2"
-  sha256 "f327eb62bfe88da335f109f28d0f8e2cd07000c548114bda41b477b0e9b23329"
+  url "http://download.drobilla.net/blop-lv2-1.0.2.tar.bz2"
+  sha256 '3e0c31ad19e144cc3e81ddb92f713062215ce7ab8382535a397d2396d7cff819'
 
   depends_on "pkg-config" => :build
   depends_on "lv2"
 
   def install
-    system "./waf", "configure", "--prefix=#{prefix}"
+    system "./waf", "configure", "--prefix=#{prefix}", "--lv2dir=#{lib}/lv2"
     system "./waf", "install"
   end
 
